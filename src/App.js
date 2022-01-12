0527c3e9c44138975002e8bc25dede3d30a8d979
yuyu.wu <yuyu.wu@opay-inc.com>
2022-01-12 16:32:02 +0800

    upd: upd

--- src/App.js
+++ src/App.js
@@ -1,30 +1,51 @@
-import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
-
+import React, { useEffect,useState } from 'react';
 // Constants
-const TWITTER_HANDLE = '_buildspace';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-
 const App = () => {
+  const [hasWallet,setWalletState] = useState(false)
+  const [publicKey,setPublicKey] = useState('')
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          setWalletState(true)
+          const response = await solana.connect({ onlyIfTrusted: true });
+          setPublicKey(response.publicKey.toString())
+        }
+      } else {
+        alert('Solana object not found! Get a Phantom Wallet 👻');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header">🖼 GIF Portal</p>
+          <p className="header">Family Pics</p>
           <p className="sub-text">
-            View your GIF collection in the metaverse ✨
+            View your family storys
           </p>
         </div>
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
-        </div>
       </div>
+      <footer className="footer-container">
+        <p className="footer-text">
+          {hasWallet?'Phantom wallet found':'Not found phantom wallet'}
+        </p>
+        <p className="footer-text">
+          Connected Public Key:{publicKey}
+        </p>
+        </footer>
     </div>
   );
 };
